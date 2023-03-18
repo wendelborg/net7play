d2bbdd36ed8fdcd2ebf811c56e3725bed4c4b2a2
lobotommy <wendelborg@gmail.com>
2023-03-18 22:03:36 +0100

    Added unit test project
    Turned off http redirect

---
+++ Net7.UnitTests/WeatherForecastTests.cs
@@ -0,0 +1,15 @@
+namespace Net7.UnitTests;
+
+public class WeatherForecastTests
+{
+    [Test]
+    public void ConvertsToFahrenheit()
+    {
+        var forecast = new WeatherForecast
+        {
+            TemperatureC = 33
+        };
+
+        Assert.That(forecast.TemperatureF, Is.EqualTo(91));
+    }
+}
